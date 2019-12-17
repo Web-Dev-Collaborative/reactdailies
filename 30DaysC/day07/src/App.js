7e2d2d55312268b4a693e77804b808f2236e1bf4
Erica L. Ingram <inquiries@aquoco.co>
2019-12-17 02:10:00 -0800

    base project files uploaded

--- 30DaysC/day07/src/App.js
+++ 30DaysC/day07/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -14,7 +13,7 @@ class App extends Component {
         <h2>Day ##: AppName</h2>
           <div className="container">
             
-            />
+            
           </div>
         </header>
       </div>
@@ -22,31 +21,3 @@ class App extends Component {
   }
 }
 export default App;
-=======
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
->>>>>>> de15466c77dab437571f651b13b1122023586d1b
